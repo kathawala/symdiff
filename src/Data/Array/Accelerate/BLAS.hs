a575be5d92c2345ae7e7e6bd8618bc93d4457688
jekbradbury <james@metamind.io>
2016-03-13 20:02:39 -0700

    FFI that aligns with the C

--- src/Data/Array/Accelerate/BLAS.hs
+++ src/Data/Array/Accelerate/BLAS.hs
@@ -1,14 +1,77 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
 module Data.Array.Accelerate.BLAS where
 
-import Foreign (Ptr)
-import Foreign.CUDA
+import Foreign
+import Foreign.Ptr
+import Foreign.C.Types
+import Foreign.CUDA hiding (Status)
+import Foreign.CUDA.Driver (initialise)
+import System.IO.Unsafe (unsafePerformIO)
+import Debug.Trace
 
-newtype Handle = Handle { useHandle :: ((Ptr ()))}
+data HandleStruct = HandleStruct
+type Handle = Ptr HandleStruct
 
-foreign import ccall "cublas_v2.h cublasCreate_v2" cublasCreate :: IO Handle
+--newtype Handle = Handle (Ptr ()) -- { useHandle :: ((Ptr ()))}
+newtype Status = Status (CInt)
+
+--instance Show Handle where
+--   show (Handle ptr) = show ptr
+
+instance Show Status where
+    show (Status status) = show status
+
+foreign import ccall "cublas_v2.h cublasCreate_v2"
+    cublasCreate :: Ptr Handle -> IO Status
+
+create :: IO Handle
+create = do
+    _ <- initialise []
+    handleptr <- malloc
+    status <- cublasCreate handleptr
+    traceShowM status
+    handle <- peek handleptr
+    return handle
+
+theHandle :: Handle
+theHandle = unsafePerformIO create
+
+--foreign import ccall "cublas.h cublasInit"
+--    cublasInit :: IO ()
+
+--foreign import ccall "cublas.h cublasSdot"
+--   cublasSdot :: Int -> DevicePtr Float
+--              -> Int -> DevicePtr Float
+--              -> Int -> DevicePtr Float -> IO ()
+
+    --alloca $ \handle -> do
+    --    status <- cublasCreate handleptr
+    --    traceShowM $ status
+    --    handle <- peek handleptr
+    --    return handle
 
 foreign import ccall "cublas_v2.h cublasSdot_v2"
    cublasSdot :: Handle 
       -> Int -> DevicePtr Float
       -> Int -> DevicePtr Float
       -> Int -> DevicePtr Float -> IO ()
+
+foreign import ccall "cublas_v2.h cublasSetPointerMode_v2"
+    cublasSetPointerMode :: Handle -> Ptr Int -> IO Status
+
+setPointerMode :: Handle -> Int -> IO ()
+setPointerMode handle pointerMode = alloca $ \pointerModePtr -> do
+    poke pointerModePtr pointerMode
+    status <- cublasSetPointerMode handle pointerModePtr
+    traceShowM status
+    return ()
+
+foreign import ccall "cublas_v2.h cublasGetPointerMode_v2"
+    cublasGetPointerMode :: Handle -> Ptr Int -> IO Status
+
+getPointerMode :: Handle -> IO Int
+getPointerMode handle = alloca $ \pointerModePtr -> do
+    status <- cublasGetPointerMode handle pointerModePtr
+    traceShowM status
+    peek pointerModePtr
--- src/Data/Array/Accelerate/BLAS/Internal/Dot.hs
+++ src/Data/Array/Accelerate/BLAS/Internal/Dot.hs
@@ -1,30 +1,32 @@
-module Data.Array.Accelerate.Symdiff where
+module Data.Array.Accelerate.BLAS.Internal.Dot where
 
+import Data.Array.Accelerate.BLAS
+import Data.Array.Accelerate.CUDA hiding (create)
 import Data.Array.Accelerate hiding (snd)
 import Data.Array.Accelerate.CUDA.Foreign
-import qualified Foreign.CUDA.Cublas as BL
 import Prelude hiding (zipWith)
-import System.Mem.Weak (addFinalizer)
-import System.IO.Unsafe (unsafePerformIO)
-import Foreign.CUDA.Driver as D
+--import System.Mem.Weak (addFinalizer)
+-- import System.IO.Unsafe (unsafePerformIO)
 import Foreign.CUDA as C
+import Debug.Trace
 
 -- D.initialise []
 -- dev <- device 0
 -- ctx <- C.create dev []
 
 
-handle :: BL.Handle
-handle = unsafePerformIO $ do
-  h <- BL.create
-  putStrLn "Initialization Complete (GPU Device found)"
-  addFinalizer handle (BL.destroy h >> putStrLn "GPU Device work complete")
-  return h
+--handle :: Handle
+--handle = unsafePerformIO $ do
+--  h <- create
+--  putStrLn "Initialization Complete (GPU Device found)"
+--  --addFinalizer handle (destroy h >> putStrLn "GPU Device work complete")
+--  return h
 
-cudaDotProductF :: (Vector Float, Vector Float) -> CIO (Scalar Float)
-cudaDotProductF (v1,v2) = do
+cudaDotProductF :: Handle -> (Vector Float, Vector Float) -> CIO (Scalar Float)
+cudaDotProductF handle (v1,v2) = do
+  traceShowM $ "cudaDotProductF"
   let n = arraySize (arrayShape v1)
-
+  traceShowM $ n
   o <- allocateArray Z
   -- need "handle" and pointer to v1 and "stride between consecutive elements of v1" and
   -- pointer to v2 and "stride between consecutive elements of v2" and result output array
@@ -33,13 +35,21 @@ cudaDotProductF (v1,v2) = do
   ((),v2ptr) <- devicePtrsOfArray v2
   ((),optr)  <- devicePtrsOfArray o
   
-  result <- liftIO $ BL.dot handle n v1ptr 1 v2ptr 1
+  traceShowM $ handle
+  traceShowM $ "got handle, calling cublasSdot"
+  liftIO $ cublasSdot handle n v1ptr 1 v2ptr 1 optr
 
-  return result
+  return o
 
 sdot :: Acc (Vector Float) -> Acc (Vector Float) -> Acc (Scalar Float)
 sdot v1 v2 = foreignAcc cudaDot pureDot $ lift (v1,v2)
-  where cudaDot = CUDAForeignAcc "cudaDotProductF" cudaDotProductF
+  where cudaDot = CUDAForeignAcc "cudaDotProductF" (\stream -> cudaDotProductF theHandle)
         pureDot :: Acc (Vector Float, Vector Float) -> Acc (Scalar Float)
         pureDot vs = let (u,v) = unlift vs
                      in fold (+) 0 $ zipWith (*) u v
+
+test = do
+  let x = fromList (Z:.10) [1..10] :: Vector Float
+  let y = fromList (Z:.10) [2..11] :: Vector Float
+
+  run $ sdot (use x) (use y)
